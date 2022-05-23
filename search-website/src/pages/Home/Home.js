315427ecddc1aa05fccbc27e8e746b3db58924d3
Vamshi Paili <vamshipaili@gmail.com>
2022-05-23 00:15:36 -0400

    Version4

--- search-website/src/pages/Home/Home.js
+++ search-website/src/pages/Home/Home.js
@@ -18,7 +18,7 @@ export default function Home() {
   return (
     <main className="main main--home">
       <div className="row home-search">
-        <img className="logo" src="https://e7.pngegg.com/pngimages/823/363/png-clipart-hologic-logo-business-business-purple-text-thumbnail.png" alt="Hologic"></img>
+        <img className="logo" src="https://www.medicaldesignandoutsourcing.com/wp-content/uploads/2018/08/hologic-MDO.jpg" alt="Hologic"></img>
         <p className="poweredby lead">Automation Hall of Fame</p>
         <SearchBar postSearchHandler={navigateToSearchPage}></SearchBar>
       </div>
